36da6d5180f7c259953f31a78f0c1f3ea8e32198
octaviantamasB <octav.tamas@gmail.com>
2016-02-06 11:14:39 +0200

    new login and register

--- src/com/Sor/User/LoginServlet.java
+++ src/com/Sor/User/LoginServlet.java
@@ -1,12 +1,20 @@
 package com.Sor.User;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 /**
  * Servlet implementation class LoginServlet
  */
@@ -25,17 +33,81 @@ public class LoginServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+			response.setContentType("text/html");  
+		    PrintWriter out = response.getWriter();  
+		          
+		    String n=request.getParameter("usr");  
+		    String p=request.getParameter("pass");  
+		    System.out.println(n);     
+		    System.out.println(p); 
+			
+			
+			
+		   if(validate(n,p)){  
+		        RequestDispatcher rd=request.getRequestDispatcher("ViewPerson.html");  
+		        rd.forward(request,response);  
+		    }  
+		    else{  
+		        out.print("Sorry username or password error");  
+		      //  RequestDispatcher rd=request.getRequestDispatcher("LoginServlet");  
+		     //   rd.include(request,response);  
+		    }  
+		         
+		    out.close();  
+	
 	}
+	private boolean validate(String username,String password) {
+		  
+
+				System.out.println("validate");
+			  
+		  return true;
+		  }
+	
+	}			  
+			  
+		/*	  URL url = new URL("http://localhost:8080/SorServer/rest/user/login");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("username", username);
+				conn.setRequestProperty("password", password);
+
+				if (conn.getResponseCode() != 200) {
+					throw new RuntimeException("Failed : HTTP error code : "
+							+ conn.getResponseCode());
+			}
+
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+
+				String output;
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					System.out.println(output);
+				}
+				conn.disconnect();
+
+			  } catch (MalformedURLException e) {
+
+				e.printStackTrace();
+
+			  } catch (IOException e) {
+
+				e.printStackTrace();
+
+			  }
+		  return false;
+
+	}*/
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+*/
 
-}
